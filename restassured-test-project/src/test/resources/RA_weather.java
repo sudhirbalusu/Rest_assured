b00df09fa415fad5392bfe681f89b05fc1c3b3f8
sudhirbalusu <sudhirbalusu@localhost>
2021-02-09 14:52:02 -0500

    First_Comit

---
+++ restassured-test-project/src/test/resources/RA_weather.java
@@ -0,0 +1,35 @@
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class RA_weather {
+  @Test
+  public void f() {
+	// Specify the base URL to the RESTful web service
+		  RestAssured.baseURI = "https://demoqa.com/utilities/weather/city";
+		  
+		  // Get the RequestSpecification of the request that you want to sent
+		  // to the server. The server is specified by the BaseURI that we have
+		  // specified in the above step.
+		  RequestSpecification httpRequest = RestAssured.given();
+		  
+		  // Make a request to the server by specifying the method Type and the method URL.
+		  // This will return the Response from the server. Store the response in a variable.
+		  Response response = httpRequest.request(Method.GET, "/Hyderabad");
+		  
+		  int respCode = response.getStatusCode();
+		  
+		  Assert.assertEquals(200, respCode, "Status code is incorrect "+ respCode);
+		  
+		  // Now let us print the body of the message to see what response
+		  // we have recieved from the server
+		  String responseBody = response.getBody().asString();
+		  System.out.println("Response Body is =>  " + responseBody);
+		  Reporter.getOutput().add(responseBody);
+  }
+}
